35a351d070a6f1578db642c09f1051fba2387c5c
YEEHAW223 <87731848+YEEHAW223@users.noreply.github.com>
2024-05-07 12:16:20 -0800

    Add files via upload

---
+++ modules in python.py
@@ -0,0 +1,180 @@
+﻿1 / 6
+
+
+
+
+# Import datetime from datetime below:
+
+
+
+
+from datetime import datetime
+
+
+
+
+current_time = datetime.now()
+
+
+print(current_time)
+
+
+
+
+2 / 6
+
+
+
+
+# Import random below:
+
+
+import random
+
+
+
+
+# Create random_list below:
+
+
+random_list = [random.randint(1,100) for i in range(101)]
+
+
+# Create randomer_number below:
+
+
+randomer_number = random.choice(random_list)
+
+
+# Print randomer_number below:
+
+
+print(randomer_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3 / 6
+
+
+import codecademylib3_seaborn
+
+
+import random
+
+
+from matplotlib import pyplot as plt
+
+
+numbers_a = range(1, 13)
+
+
+numbers_b = random.sample(range(1000), 12)
+
+
+print(numbers_b)
+
+
+plt.plot(numbers_a, numbers_b)
+
+
+plt.show()
+
+
+# Add your code below:
+
+
+________________
+
+
+4 / 6
+
+
+
+
+# Import Decimal below:
+
+
+from decimal import Decimal
+
+
+# Fix the floating point math below:
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
+
+
+
+
+5 / 6
+
+
+
+
+script.py
+
+
+from library import always_three
+
+
+#from sandwiches import sandwiches
+
+
+
+
+
+
+# Call your function below:
+
+
+
+
+always_three()
+
+
+
+
+library.py
+
+
+from library import always_three
+
+
+#from sandwiches import sandwiches
+
+
+
+
+
+
+# Call your function below:
+
+
+
+
+always_three()
+6 / 6 
+
+
+Modules Python Review
